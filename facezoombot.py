6bdbeef6376491fe4aac4cdfb984d7f674c5efa9
Jelle Besseling <jelle@pingiun.com>
2016-10-29 07:50:21 -0400

    Fix bot by using self instead of bot

--- facezoombot.py
+++ facezoombot.py
@@ -3,6 +3,8 @@
 import telepot
 import requests
 
+import configparser
+
 from PIL import Image
 
 class ZoomBot(telepot.Bot):
@@ -17,29 +19,29 @@ class ZoomBot(telepot.Bot):
         content_type, chat_type, chat_id = telepot.glance(msg)
         if content_type == 'text':
             if msg['text'] == '/start' or msg['text'].startswith('/help'):
-                bot.sendMessage(chat_id, self.helptext)
+                self.sendMessage(chat_id, self.helptext)
             if msg['text'] == '/about':
-                bot.sendMessage(chat_id, "This bot is made by Jelle Besseling (@pingiun)\nHosted by @bothosting.")
+                self.sendMessage(chat_id, "This bot is made by Jelle Besseling (@pingiun)\nHosted by @bothosting.")
             elif chat_type == 'private':
-                bot.sendMessage(chat_id, self.helptext)
+                self.sendMessage(chat_id, self.helptext)
         elif content_type in ['new_chat_participant', 'left_chat_participant', 'new_chat_title', 'new_chat_photo', 'delete_chat_photo', 'group_chat_created', 'supergroup_chat_created', 'migrate_to_chat_id', 'migrate_from_chat_id', 'channel_chat_created']:
             return
         elif content_type == 'photo':
-            bot.sendChatAction(chat_id, 'upload_photo')
+            self.sendChatAction(chat_id, 'upload_photo')
             file_path = '/tmp/' + msg['photo'][-1]['file_id'] + '.jpg'
             print("Downloading file")
-            bot.download_file(msg['photo'][-1]['file_id'], file_path)
+            self.download_file(msg['photo'][-1]['file_id'], file_path)
             print("Recognizing faces")
             params = {'image': open(file_path, 'rb')}
-            headers = {'X-Mashape-Key': TOKEN_MA, 'Accept': 'application/json'}
+            headers = {'X-Mashape-Key': self.TOKEN_MA, 'Accept': 'application/json'}
             request = requests.post('https://apicloud-facerect.p.mashape.com/process-file.json', files=params, headers=headers)
 
             faces = request.json()
             
             if len(faces['faces']) > 1 and chat_type == 'private':
-                bot.sendMessage(chat_id, "There are too many faces in your photo, I only work when one face is visible. Please crop your foto to only show one face.")
+                self.sendMessage(chat_id, "There are too many faces in your photo, I only work when one face is visible. Please crop your foto to only show one face.")
             if len(faces['faces']) == 0 and chat_type == 'private':
-                bot.sendMessage(chat_id, "Couldn't detect a face in your picture.")
+                self.sendMessage(chat_id, "Couldn't detect a face in your picture.")
             else:
                 face = faces['faces'][0]
                 im = Image.open(file_path)
@@ -61,10 +63,10 @@ class ZoomBot(telepot.Bot):
                 crop = im.crop((face['x'] + dwidthleft, face['y'] + dheighttop, face['x'] + face['width'] - dwidthright, face['y'] + face['height'] - dheightbottom))
                 crop_path = '/tmp/' + msg['photo'][-1]['file_id'] + 'zoom.jpg'
                 crop.save(crop_path, 'JPEG')
-                bot.sendPhoto(chat_id, open(crop_path, 'rb'))
+                self.sendPhoto(chat_id, open(crop_path, 'rb'))
         else:
             if chat_type == 'private':
-                bot.sendMessage(chat_id, "I don't get it. Use /help to find out how this bot works.")
+                self.sendMessage(chat_id, "I don't get it. Use /help to find out how this bot works.")
 
 def main():
     configfile = configparser.ConfigParser()
@@ -73,4 +75,8 @@ def main():
     TOKEN_MA = configfile['bot']['MashapeToken']
     
     bot = ZoomBot(TOKEN_TG)
+    bot.TOKEN_MA = TOKEN_MA
     bot.message_loop(run_forever=True)
+
+if __name__ == '__main__':
+    main()
